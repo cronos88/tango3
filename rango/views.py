31ea6c893b10fd4a8580a49785ed4f9f6fd18126
cronos88 <atlantis9@hotmail.com>
2015-01-13 22:49:00 -0500

    media statics y templates

---
+++ rango/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+
+	context_dict = {'boldmessage': 'Yo soy una fuente negrita desde el context'}
+	return render(request, 'rango/index.html', context_dict)
+	
+
+def about(request):
+
+	return render(request, 'rango/about.html')
+
+
+
--- tango/settings.py
+++ tango/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+
+    os.path.join(BASE_DIR, 'templates'),
+
+)
+
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR, 'static'),
+
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
