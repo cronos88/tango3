65ba3eb755dc8df36e08beb384e782c7a086f1d1
cronos88 <atlantis9@hotmail.com>
2015-01-15 21:55:41 -0500

    creacion modelos Category y Page

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Page
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+	
+	fields = ['name', 'views', 'likes',]
+
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('category', 'title', 'url')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+
+
